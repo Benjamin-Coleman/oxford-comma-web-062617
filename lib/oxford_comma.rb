a0eac387ea0a30c4f846d74897ad8975b83ba704
Benjamin Coleman <hello@benjamincoleman.me>
2017-06-16 17:04:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    array.join(' and ')
+  else
+    last_element = array.pop
+    #puts array.inspect
+    array.join(', ').concat(", and #{last_element}")
+  end
 end
+
+#oxford_comma(['steph','kd','klay','dray'])
